15a06620817b096d9a6676d892a83ce2c112d213
salvaalejos <alejossalvador@gmail.com>
2025-11-18 11:31:05 -0600

    nose

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 // 1. Definimos la URL base de nuestra API
-const baseURL = 'https://localhost:5000/api';
+const baseURL = 'http://localhost:5000/api';
 
 // 2. Creamos una "instancia" de axios
 const api = axios.create({
   baseURL: baseURL,
-  withCredentials: true  // <-- ¡ESTA ES LA MAGIA!
+  withCredentials: true
 });
 
 // 3. Exportamos esta instancia para usarla en toda la app
